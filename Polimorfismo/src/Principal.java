9bb64695ac325fe2c900227118d385ca5eb0383c
daflores95 <da_flores95@hotmail.com>
2015-06-05 09:08:44 -0500

    Polimorfismo
    Video

---
+++ Polimorfismo/src/Principal.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Diego on 6/5/2015.
+ */
+public class Principal {
+    public static void main(String[] args){
+        Numero num = new Numero();
+        num.set(10,5.5);
+        System.out.println("El valor de los atributos: num1 ="+num.getNum1()+"num2 ="+num.getNum2());
+    }
+}
